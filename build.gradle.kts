b4a9e1f5537247f0ba79a0c9a70caf9bde4281e0
piacenti <piacenti@users.noreply.github.com>
2022-06-08 23:11:36 -0700

    updating versions and adding serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@
 val kotest_version: String by project
 
 plugins {
-    kotlin("multiplatform") version "1.5.20"
+    kotlin("multiplatform") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
 }
 
 group = "org.example"
@@ -28,11 +29,12 @@ kotlin {
             dependencies {
                 implementation(kotlin("stdlib"))
                 implementation(kotlin("reflect"))
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
             }
         }
         val commonTest by getting {
             dependencies {
-//                implementation("io.kotest:kotest-famework:$kotest_version")
+//              implementation("io.kotest:kotest-famework:$kotest_version")
                 implementation("io.kotest:kotest-assertions-core:$kotest_version")
                 implementation("io.kotest:kotest-property:$kotest_version")
                 implementation("io.kotest:kotest-framework-engine:$kotest_version")
